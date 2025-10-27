4aca67460566ab995228e25c88386eec63a6a142
W4RSH3LL <129652925+W4RSH3LL@users.noreply.github.com>
2025-10-27 11:12:47 +0100

    Update index.php

--- orange_free_wifi/index.php
+++ orange_free_wifi/index.php
@@ -4,7 +4,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Portail Captif - Accès invité</title>
-<script src="https://cdn.tailwindcss.com"></script>
+<script src="tailwind.js"></script>
 <script>
   tailwind.config = {
     theme: {
@@ -194,3 +194,4 @@
 </script>
 </body>
 </html>
+
